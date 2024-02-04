44fe9aa2f316aa1f93d62d3c62b4adf59b981393
6652410018GittiWilawan <s6652410018@sau.ac.th>
2024-02-04 11:44:58 +0700

    add delete api

---
+++ apis/user/delete_user_api.php
@@ -0,0 +1,28 @@
+<?php
+header("Access-control-allow-origin: *");
+header("content-type: application/json; charset=UTF-8");
+
+include_once "./../../databaseconnect.php";
+include_once "./../../models/user.php";
+
+$databaseConnect = new DatabaseConnect();
+$connDB = $databaseConnect->getConnection();
+$user = new user($connDB);
+
+//สร้างตัวแปรเก็บค่าของข้อมูลที่ส่งมาซึ่งเป็น JSON ที่ทำการ decode แล้ว
+$data = json_decode(file_get_contents("php://input"));
+
+//เอาข้อมูลที่ถูก Decode ไปเก็บในตัวแปร
+$user->user_id = $data->user_id;
+
+
+//เรียกใช้ Function ตามวัตถุประสงค์ของ API ตัวนี้
+if($stmt = $user->deleteUser()){
+    //แก้ไขข้อมูลสำเร็จ
+   http_response_code(200);
+   echo json_encode(array("message"=>"1")); 
+}else{
+    //แก้ไขข้อมูลไม่สำเร็จ
+    http_response_code(200);
+    echo json_encode(array("message"=>"0"));     
+}
